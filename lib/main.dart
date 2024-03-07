80a86b855b199508a66e3858e7f7294002b71990
Shlok <notshlokmestry@gmail.com>
2024-03-07 16:35:53 +0000

    Setting up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:mindfulmoments/screens/onboarding.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main(){
+
+Future<void> main() async {
   runApp(const MyApp());
+  await Firebase.initializeApp(
+
+    options: DefaultFirebaseOptions.currentPlatform,
+
+);
+  
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
